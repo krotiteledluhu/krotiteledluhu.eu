fb1d58a53c29dfe62725d8d0e77f546ca2580142
Radim Klaska <radim@klaska.net>
2016-07-14 16:14:15 +0200

    Revert "test js datepicker"
    This reverts commit 26b823e655d820a7f773f3065d85afb946d8ff22.

--- js/app.js
+++ js/app.js
@@ -38,14 +38,17 @@ $window = $(window),
 $review_image;
 
 $document.ready(() => {
+	$('.datepicker').datepicker();
+	/*
 	var $dtpckr = $('.datepicker'),
-
+	*/
+	 */
 		$bodyhtml = $('html, body');
-	$dtpckr.datepicker({
+	/* $dtpckr.datepicker({
 		language: 'cs'
 	});
 
-	 /*
+
 	$dtpckr.next('.input-group-addon').click((e) => {
 		e.preventDefault();
 		$dtpckr.datepicker('show');
